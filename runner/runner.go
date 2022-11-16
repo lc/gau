b1e8ca700e943e4bcf4073baa14eaf7c860bc16c
Florian Pfitzer <florian@pfitzer.me>
2022-11-16 10:58:32 +0100

    expose runner cancel func

--- runner/runner.go
+++ runner/runner.go
@@ -65,6 +65,11 @@ func (r *Runner) Wait() {
 	r.wg.Wait()
 }
 
+// Cancel fetching from providers
+func (r *Runner) Cancel() {
+	r.cancelFunc()
+}
+
 // worker checks to see if the context is finished and executes the fetching process for each provider
 func (r *Runner) worker(ctx context.Context, domains chan string, results chan string) {
 work:
